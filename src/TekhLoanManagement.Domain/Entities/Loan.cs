a95924f02e73e39443c377a62dd085437d6d237d
ehsan_arefzadeh <arefzaadeh@gmail.com>
2026-01-05 18:02:48 +0330

    Add Entities And DbContext

---
+++ src/TekhLoanManagement.Domain/Entities/Loan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TekhLoanManagement.Domain.Abstractions;
+
+namespace TekhLoanManagement.Domain.Entities
+{
+    public class Loan : BaseEntity<Guid>
+    {
+        public DateOnly StartDate { get; set; }
+        public Guid MemberId { get; set; }
+        public Member Member { get; set; }
+        public Guid FundId { get; set; }
+        public Fund Fund { get; set; }
+        public decimal Amount { get; set; }
+        public int InstallmentCount { get; set; }
+        ICollection<Installment> Installments { get; set; } = new List<Installment>();
+    }
+}
---
+++ src/TekhLoanManagement.Infrastructure/Context/TekhLoanDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TekhLoanManagement.Domain.Entities;
+
+namespace TekhLoanManagement.Infrastructure.Context
+{
+
+    public class TekhLoanDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public TekhLoanDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected TekhLoanDbContext()
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.DebitWalletAccount)
+                .WithMany(a => a.DebitTransactions)
+                .HasForeignKey(t => t.DebitWalletAccountId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Transaction>()
+                .HasOne(t => t.CreditWalletAccount)
+                .WithMany(a => a.CreditTransactions)
+                .HasForeignKey(t => t.CreditWalletAccountId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<WalletAccount>()
+                .HasOne(t => t.Member)
+                .WithOne(a => a.WalletAccount)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<WalletAccount>()
+               .HasOne(t => t.Fund)
+               .WithOne(a => a.WalletAccount)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        }
+        public DbSet<Fund> Funds { get; set; } = default!;
+        public DbSet<Installment> Installments { get; set; } = default!;
+        public DbSet<Loan> Loans { get; set; } = default!;
+        public DbSet<Member> Members { get; set; } = default!;
+        public DbSet<User> User { get; set; } = default!;
+        public DbSet<Transaction> Transactions { get; set; } = default!;
+        public DbSet<WalletAccount> walletAccounts { get; set; } = default!;
+    }
+}
