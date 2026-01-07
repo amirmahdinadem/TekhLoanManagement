ae893731997ffa8ff6b0b94615dd50b0564f9300
ehsan_arefzadeh <arefzaadeh@gmail.com>
2026-01-07 14:46:02 +0330

    Add Temp Exception, Add WalletAccount Domain Method

--- src/TekhLoanManagement.Domain/Entities/WalletAccount.cs
+++ src/TekhLoanManagement.Domain/Entities/WalletAccount.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using TekhLoanManagement.Domain.Abstractions;
 using TekhLoanManagement.Domain.Enums;
+using TekhLoanManagement.Domain.Exceptions;
 
 namespace TekhLoanManagement.Domain.Entities
 {
@@ -15,6 +16,31 @@ namespace TekhLoanManagement.Domain.Entities
         public ICollection<Transaction>? CreditTransactions { get; set; } = new List<Transaction>();
         public Member? Member { get; set; }
         public Fund? Fund { get; set; }
+
+        public void Debit(decimal amount)
+        {
+            if (Status != WalletAccountStatus.Active)
+                throw new DomainException("Account is not active");
+
+            if (amount <= 0)
+                throw new DomainException("Invalid amount");
+
+            if (Balance < amount)
+                throw new DomainException("Insufficient funds");
+
+            Balance -= amount;
+        }
+
+        public void Credit(decimal amount)
+        {
+            if (Status != WalletAccountStatus.Active)
+                throw new DomainException("Account is not active");
+
+            if (amount <= 0)
+                throw new DomainException("Invalid amount");
+
+            Balance += amount;
+        }
 
     }
 }
---
+++ src/TekhLoanManagement.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TekhLoanManagement.Domain.Exceptions
+{
+    public sealed class DomainException : Exception
+    {
+        public DomainException(string message)
+            : base(message)
+        {
+        }
+    }
+}
