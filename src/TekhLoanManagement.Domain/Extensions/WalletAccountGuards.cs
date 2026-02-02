068166552c61f5d7745faf8aa271fab0a97f4cea
Ehsan Arefzadeh <arefzaadeh@gmail.com>
2026-02-02 14:12:23 +0330

    Add Frozen In Wallet Account

---
+++ src/TekhLoanManagement.Domain/Extensions/WalletAccountGuards.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TekhLoanManagement.Domain.Entities;
+using TekhLoanManagement.Domain.Enums;
+using TekhLoanManagement.Domain.Exceptions;
+
+namespace TekhLoanManagement.Domain.Extensions
+{
+    public static class WalletAccountGuards
+    {
+        public static void Debit(this IGuardClause guardClause, WalletAccount account, decimal amount)
+        {
+            if (account.Status != WalletAccountStatus.Active)
+                throw new DomainException("Account is not active");
+
+            if (amount <= 0)
+                throw new DomainException("Invalid amount");
+
+            if (account.GetBalance() < amount)
+                throw new DomainException("Insufficient funds");
+        }
+
+        public static void Credit(this IGuardClause guardClause, WalletAccount account, decimal amount)
+        {
+            if (account.Status != WalletAccountStatus.Active)
+                throw new DomainException("Account is not active");
+
+            if (amount <= 0)
+                throw new DomainException("Invalid amount");
+        }
+    }
+}
--- src/TekhLoanManagement.Infrastructure/Migrations/20260202103423_EhsanFix2.Designer.cs
+++ src/TekhLoanManagement.Infrastructure/Migrations/20260202103423_EhsanFix2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TekhLoanManagement.Infrastructure.Context;
 
@@ -11,9 +12,11 @@ using TekhLoanManagement.Infrastructure.Context;
 namespace TekhLoanManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(TekhLoanDbContext))]
-    partial class TekhLoanDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260202103423_EhsanFix2")]
+    partial class EhsanFix2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
